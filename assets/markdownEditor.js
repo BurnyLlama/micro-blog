7a25895f2e790657aa34a83032d191cf6b4f4aab
Fabian Seluska <fabianseluska@gmail.com>
2022-09-01 08:29:36 +0200

    Fix editors.

--- assets/markdownEditor.js
+++ assets/markdownEditor.js
@@ -1,28 +1,42 @@
-const textarea = document.querySelector("#blog-post-body")
-const preview  = document.querySelector("#markdown-preview")
+/**
+ * Sets up a markdown editor
+ * @param {Element} textarea The element to get the text from.
+ * @param {Element} preview The element to preview/syntaxhighlight to.
+ */
+function setupMarkdownEditor(textarea, preview) {
+    textarea.style.color = "transparent"
 
-textarea.style.color = "transparent"
+    textarea.addEventListener(
+        "input",
+        () => {
+            const currentText = textarea.value
 
-textarea.addEventListener(
-    "input",
-    () => {
-        const currentText = textarea.value
+            const syntaxHighlighted = currentText
+                // This is important for scrolling...
+                .replace(/\n$/, "\n ")
+                // This is for headings...
+                .replace(/^(#+)(?= )/gm, "<span class=\"muted\">$&</span>")
+                .replace(/^/gm, "<span class=\"line\">")
+                .replace(/$/gm, "</span>")
 
-        const syntaxHighlighted = currentText
-            // This is important for scrolling...
-            .replace(/\n$/, "\n ")
-            // This is for headings...
-            .replace(/^(#+)(?= )/gm, "<span class=\"muted\">$&</span>")
-            .replace(/^/gm, "<span class=\"line\">")
-            .replace(/$/gm, "</span>")
+            preview.innerHTML = syntaxHighlighted
+        }
+    )
 
-        preview.innerHTML = syntaxHighlighted
-    }
+    // Run an initial event...
+    textarea.dispatchEvent(new Event("input"))
+
+    textarea.addEventListener("scroll", () => {
+        preview.scrollTop = textarea.scrollTop
+    })
+}
+
+setupMarkdownEditor(
+    document.querySelector("#blog-post-body"),
+    document.querySelector("#blog-post-preview")
 )
 
-// Run an initial event...
-textarea.dispatchEvent(new Event("input"))
-
-textarea.addEventListener("scroll", () => {
-    preview.scrollTop = textarea.scrollTop
-})
+setupMarkdownEditor(
+    document.querySelector("#about-text"),
+    document.querySelector("#about-text-preview")
+)
